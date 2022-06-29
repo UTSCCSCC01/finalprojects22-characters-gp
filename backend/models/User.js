be31fc038b0b5a5e6f486517e7e6752e0c788b0a
Eric Zhou <ericckzhou@gmail.com>
2022-06-29 16:37:29 -0400

    CHAR-12/CHAR-56 - Updated User Schema (fixed commit msg)

--- backend/models/User.js
+++ backend/models/User.js
@@ -7,10 +7,9 @@ let userSchema = new Schema({
   password: {
     type: String
   },
-  type: {
-    type: String,
-    default: '1'
+  isEmployer: {
+    type: Boolean,
+    default: false
   }
-  //1 = normal user, 2 = character, 3 = employee
 })
 module.exports = mongoose.model('User', userSchema)
